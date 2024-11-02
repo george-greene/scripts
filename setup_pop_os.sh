349c6a773d7607de97a9e5c59bc1dd3a687b32d3
George Greene <170787498+george-greene@users.noreply.github.com>
2024-11-01 20:04:09 -0400

    feat: brave browser nightly

--- setup_pop_os.sh
+++ setup_pop_os.sh
@@ -88,7 +88,15 @@ fi
 
 if ! command -v brave-browser-nightly 2>&1 >/dev/null
 then
-	echo
+	sudo apt install curl
+
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
+
+	echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg] https://brave-browser-apt-nightly.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-nightly.list
+
+	sudo apt update
+
+	sudo apt install brave-browser-nightly
 fi
 
 if ! command -v gh 2>&1 >/dev/null
