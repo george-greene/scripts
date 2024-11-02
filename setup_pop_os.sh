6f30d325c277be5a8c6d5e8d61d8f0401f719085
George Greene <170787498+george-greene@users.noreply.github.com>
2024-11-01 20:00:54 -0400

    feat: gh

--- setup_pop_os.sh
+++ setup_pop_os.sh
@@ -93,7 +93,13 @@ fi
 
 if ! command -v gh 2>&1 >/dev/null
 then
-	echo
+	(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+		&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+		&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+		&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+		&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+		&& sudo apt update \
+		&& sudo apt install gh -y
 fi
 
 if ! command -v ghcup 2>&1 >/dev/null
